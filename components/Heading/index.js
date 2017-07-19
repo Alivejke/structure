e75c03858f7ada9dc3276637ca85f372787cc12a
Dmytro Baltak <d.baltak@tmg.nl>
2017-07-19 15:23:41 +0200

    Add nested comp

---
+++ components/Heading/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import styles from './heading.css';
+
+class Heading extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  getTitle() {
+    const { title } = this.props;
+
+    return (
+      ... some complex structure ...
+      <div>
+        <div>
+          {title}
+        </div>
+      </div>
+    );
+  }
+
+  getChapeau() {
+    const { chapeau } = this.props;
+
+    return (
+      ... some complex structure ...
+      <div>
+        <div>
+          {title}
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className={classes}>
+        {nodeChapeau}
+        {nodeTitle}
+      </div>
+    );
+  }
+}
+
+Heading.propTypes = {
+  title: React.PropTypes.string.isRequired,
+  chapeau: React.PropTypes.string.isRequired,
+};
+
+export default Heading;
